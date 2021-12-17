b43e4d47e549f5c55c04911a754aa5f44dc2585d
Gustavo Henrique <gustavo._henrique@hotmail.com>
2021-12-17 11:41:11 -0300

    Troca do texto de apresentação

--- pages/api/userData.ts
+++ pages/api/userData.ts
@@ -8,7 +8,10 @@ const user: User = {
   name: "Gustavo Henrique",
   summary: [
     "Estudante de Engenharia da Computação.",
-    "Gosto de aprender coisas novas, de colocar a mão na massa e ajudar meus colegas de trabalho com o que sei. Quero estar sempre imerso em uma cultura inovadora e crescendo a cada dia mais. Gosto de trabalhar com novas tecnologias, programação, banco de dados e tratamento de dados.",
+    `Apaixonado por desenvolvimento, enfrentar desafios e compartilhar conhecimento.
+    Experiência em NodeJS, Html, CSS, Javascript, MongoDB, SQLServer, Oracle, Docker, metodologia de Kanban.
+    
+    Quero fazer a diferença e desenvolver soluções com valores agregados para empresa/cliente!`,
   ],
 };
 

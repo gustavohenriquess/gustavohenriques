73427c4bf02ae10a4a8b14602a5620d6199d6013
Gustavo Henrique <gustavo._henrique@hotmail.com>
2024-03-21 17:13:40 -0300

    feat: update data

--- pages/api/experiences.ts
+++ pages/api/experiences.ts
@@ -7,14 +7,26 @@ const experiences: Experience[] = [{
     translate: {
       pt: {
         title: "Application Development Analyst",
-        description: ['Atuando como líder técnico em uma equipe multi funções, tenho como atribuição auxiliar nas definições de prioridades, estimativas de novos projetos e apoiando o PM e SM.',
-          'Como desenvolvedor backend atuo em projetos nodeJS, serverless, SNS, SQS, MongoDB e AWS.'
-        ],
+        description: [
+          `Analista de Desenvolvimento atuando como Desenvolvedor Backend em projetos na Sky.`,
+          `Atuo desempenhando um papel fundamental na minha equipe liderando o time tecnicamente e como desenvolvedor Backend na criação de novos projetos. Apoio aos PM, PO e SM para análise de projetos, levantamento de requisitos, identificação de possíveis desafios antes que se tornem um problema, organização das tarefas e distribuição delas.`,
+          `Dentro dos 7 projetos já efetuados no cliente utilizei as seguintes tecnologias:
+            AWS (Lambda, EC2, DynamoDB, S3, SQS, SNS, CloudWatch), 
+            NodeJS, 
+            Jenkins, DataDog, Splunk, 
+            MongoDB`,  
+      ],
       },
       en: {
         title: "Application Development Analyst",
-        description: ['Acting as a technical leader in a multi-function team, I have the task of assisting in defining priorities, estimating new projects and supporting the PM and SM.',
-          'As a backend developer I work on projects with nodeJS, serverless, SNS, SQS, MongoDB and AWS.'
+        description: [
+          `Development Analyst working as Backend Developer on projects at Sky.`,
+          `I play a fundamental role in my team, leading the team technically and as a Backend developer in the creation of new projects. Support for PM, PO and SM to analyze projects, gather requirements, identify possible challenges before they become a problem, organize tasks and distribute them.`,
+          `Within the 7 projects already carried out at the client, I used the following technologies: 
+          AWS (Lambda, EC2, DynamoDB, S3, SQS, SNS, CloudWatch), 
+          NodeJS, 
+          Jenkins, DataDog, Splunk, 
+          MongoDB`,
         ],
       },
     },
@@ -25,15 +37,24 @@ const experiences: Experience[] = [{
       pt: {
         title: "Analista de Sistemas Júnior",
         description: [
-          "Na área de Pesquisa e Desenvolvimento, junto aos times de Roadmap e Sustentação.",
-          "Utilizo as tecnologias Javascript (Frontend e Backend), HTML, CSS, Jquery, Banco de Dados SQL Server, Oracle, Docker e Kanban.",
+          `Na área de Pesquisa e Desenvolvimento, junto aos times de Roadmap e Sustentação.`,
+          `Junto ao time de Roadmap desenvolvo novas funcionalidades do CRM, e refatoramos o produto utilizando tecnologias low code e alteramos a arquiterura para uma baseada em microsserviços.`,
+          `Junto ao time de Sustentação faço analises e desenvolvimento de erros encontrados por QA e Clientes.`,
+          `Ajudo a efetuar deploy, criações de APIs no padrão Restful e tive oportunidades de planejar novas funcionalidades e melhorias do zero.`,
+          `Tive a oportunidade de desenvolver uma POC para uma automatizar o processo de testes da nossa aplicação.`,
+          `Utilizo Javascript (Frontend e Backend), NodeJS, HTML, CSS, Jquery, Banco de Dados SQL Server, Oracle e MongoDB e metodologia Kanban.`,
+
         ],
       },
       en: {
         title: "Junior System Analyst",
         description: [
-          "In the Research and Development area, together with the Roadmap and Support teams.",
-          "I use Javascript technologies (Frontend and Backend), HTML, CSS, Jquery, SQL Server Database, Oracle, Docker and Kanban.",
+          `In the Research and Development area, together with the Roadmap and Support teams.`,
+          `Together with the Roadmap team, I develop new CRM features, and we refactor the product using low code technologies and change the architecture to one based on microservices.`,
+          `Together with the Support team, I analyze and develop errors found by QA and Customers.`,
+          `I help deploy, create APIs using the Restful standard and have the opportunity to plan new features and improvements from scratch.`,
+          `I had the opportunity to develop a POC to automate the testing process of our application.`,
+          `I use Javascript (Frontend and Backend), NodeJS, HTML, CSS, Jquery, SQL Server Database, Oracle and MongoDB and Kanban methodology.`,
         ],
       },
     },
@@ -44,13 +65,13 @@ const experiences: Experience[] = [{
       pt: {
         title: "Estágio em infraestrutura e Redes",
         description: [
-          "Responsável pela infraestrutura de servidores, Servidores windows, redes, suporte técnico, gestão dos equipamentos da empresa e controle de backup.",
+          "Responsável pela Infraestrutura de Servidores, Servidores Windows, redes, suporte técnico, gestão dos equipamentos da empresa e controle de backup.",
         ],
       },
       en: {
         title: "Internship in Infrastructure and Networks",
         description: [
-          "Responsible for server infrastructure, Windows servers, networks, technical support, management of company equipment and backup control.",
+          "Responsible for Server Infrastructure, Windows Servers, networks, technical support, management of company equipment and backup control.",
         ],
       },
     },
@@ -61,13 +82,13 @@ const experiences: Experience[] = [{
       pt: {
         title: "Estágio em infraestrutura e Redes",
         description: [
-          "Servidores linux e windows, Voip, rotas de ligações, Infraestrutura de servidores, e Reparos em geral de máquinas,analise e criação de relatórios.",
+          "Servidores Linux e Windows, Voip, rotas de Ligações, Infraestrutura de servidores, e reparos em geral de máquinas, análise e criação de relatórios.",
         ],
       },
       en: {
         title: "Internship in Infrastructure and Networks",
         description: [
-          "Linux and Windows servers, Voip, Call routes, Server Infrastructure, and General machine repairs, Analysis and reporting.",
+          "Linux and Windows servers, VoIP, call routes, server infrastructure, and general machine repairs, analysis and creation of reports.",
         ],
       },
     },
@@ -84,7 +105,7 @@ const experiences: Experience[] = [{
       en: {
         title: "Level 1 Analyst",
         description: [
-          "Customer service within the company Via Varejo, Linux servers, Voip and PABX calls and remote support.",
+          "Support for users within the company Via Varejo, Linux Servers, VoIP and PBX calls and remote support.",
         ],
       },
     },
--- pages/api/userData.ts
+++ pages/api/userData.ts
@@ -9,20 +9,22 @@ const user: User = {
       alt_image: "Website logo",
       office: "Engenheiro da computação | Desenvolvedor backend NodeJS",
       summary: [
-        `Apaixonado por desenvolvimento, aprender, enfrentar desafios e compartilhar conhecimento.`,
-        `Experiência em NodeJS, Html, CSS, Javascript, MongoDB, SQLServer, Oracle, Docker, metodologia de Kanban.`,
-        `Atual foco em arquitetura de projetos, desenvolvimento em NodeJS com TypeScript e aprendendo GoLang nas horas vagas.
-        Quero fazer a diferença e desenvolver soluções com valores agregados para empresa/cliente!`,
+        `Desenvolvedor a 6 anos, trabalhando com Javascript, principalmente NodeJS.`,
+        `Durante esses anos adquiri diversas habilidades, nas areas de tecnologia, ferramentas e gestão. Trabalhando em consultoria e no desenvolvimento de CRM.`,
+        `Participei de projetos de impacto em ambas as empresas onde desenvolvi software de chamados, e-mail, vendas de produtos, chat, Gift Card, Bots para Whatsapp e Telegram.`,
+        `Nos últimos dois anos, foquei em evoluir e assumir mais responsabilidades nos projetos em que trabalhei, buscando fazer a diferença tanto para a empresa quanto para o time.`,
+
       ],
     },
     en: {
       alt_image: "Website logo",
       office: "Computer engineer | NodeJS Backend developer",
       summary: [
-        `Passionate about development, learning, facing challenges and sharing knowledge.`,
-        `Experience in NodeJS, Html, CSS, Javascript, MongoDB, SQLServer, Oracle, Docker, Kanban methodology.`,
-        `Current focus on project architecture, NodeJS development with TypeScript and learning GoLang in my spare time.
-        I want to make a difference and develop value-added solutions for the company/client!`,
+        `Developer for 6 years, working with Javascript, mainly NodeJS.`,
+        `During these years I acquired several skills, in the areas of technology, tools and management.`,
+        `I worked in consultancy and CRM development.`,
+        `I participated in impact projects in both companies where I developed software for calls, email, product sales, chat, Gift Card, Bots for Whatsapp and Telegram.`,
+        `In the last two years, I focused on evolving and taking on more responsibilities in the projects I worked on, seeking to make a difference for both the company and the team.`,
       ],
     },
   },
